f21c86a5361c20ac3736f595c06b312a330a4672
Anthony Ikeda <anthony.ikeda@cognizant.com>
2020-02-28 15:42:36 -0800

    Refined the database tables to proper naming conventions

---
+++ Migrations/20200228234133_initial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace customer_api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customer",
+                columns: table => new
+                {
+                    customer_id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    firstname = table.Column<string>(nullable: false),
+                    lastname = table.Column<string>(nullable: true),
+                    email_address = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_customer", x => x.customer_id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customer");
+        }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,18 +1,24 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace customer_api.Models
 {
+    [Table("customer")]
     public class Customer
     {
+        [Column("customer_id")]
         public long CustomerId { get; set; }
 
         [Required]
+        [Column("firstname")]
         public String Firstname { get; set; }
 
+        [Column("lastname")]
         public String Lastname { get; set; }
 
         [Required]
+        [Column("email_address")]
         public String EmailAddress { get; set; }
     }
 }
